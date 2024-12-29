2e0429324a6a5bb79104eaf1ba1e2439ff79d602
fadymoh <fadymohamedauc@aucegypt.edu>
2024-12-29 16:55:20 +0200

    Upgrade React to latest

--- User_Frontend_Service/src/index.js
+++ User_Frontend_Service/src/index.js
@@ -1,5 +1,10 @@
-import ReactDOM from "react-dom";
 import App from "./App";
 import React from "react"
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
