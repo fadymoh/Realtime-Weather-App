ea21aa0c39984cdc8fcebb01952a6513f693f3e8
fadymoh <fadymohamedauc@aucegypt.edu>
2024-12-29 21:13:58 +0200

    Add HTTPS Support for Backend Service

--- User_Facing_Backend_Service/server.js
+++ User_Facing_Backend_Service/server.js
@@ -1,17 +1,22 @@
-import http from 'http'
+import { readFileSync } from 'fs';
+import https from 'https'
 import cluster from 'cluster'
 import os from 'os'
 import app from './app.js'
 import logger from './helpers/Logger.js'
 
-if (cluster.isPrimary)
-{
+// HTTPS Certificate
+const options = {
+    key: readFileSync('key.pem'),
+    cert: readFileSync('cert.pem')
+};
+
+if (cluster.isPrimary) {
     logger.log('debug', "Master " + process.pid + " is running");
 
     let cpuCount = os.cpus().length / 4;
 
-    for (let i = 1; i <= cpuCount; i++)
-    {
+    for (let i = 1; i <= cpuCount; i++) {
         const portNumber = parseInt(process.env.PORT) + 1;
         process.env.PORT = portNumber;
         cluster.fork();
@@ -22,9 +27,8 @@ if (cluster.isPrimary)
         cluster.fork();
     });
 }
-else
-{
-    const server = http.createServer(app);
+else {
+    const server = https.createServer(options, app);
     server.listen(process.env.PORT, () => {
         logger.log('debug', `Example app listening on port ${process.env.PORT}`)
     })
